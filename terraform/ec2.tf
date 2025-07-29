132d1bdab2961a72a507f7a30bacf09f4da4071c
diegocrocha <61556716+diegocrocha@users.noreply.github.com>
2025-07-29 17:35:26 -0300

    Add Kafka EC2 and outputs

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "ec2" {
     Name = "ec2-linux"
   }
 }
+
+resource "aws_instance" "kafka" {
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = aws_key_pair.generated_key.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "kafka-broker"
+  }
+}
