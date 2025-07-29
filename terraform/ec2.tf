8778783ae33101b8bf1d96ca77f7f67a4ecca006
diegocrocha <61556716+diegocrocha@users.noreply.github.com>
2025-07-29 17:39:45 -0300

    Use variable for EC2 AMI

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                         = "ami-0c02fb55956c7d316"
+  ami                         = var.ami
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
